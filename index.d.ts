f98a2ddacddad206c80890c7f54e35ea3bfe8958
YevhenKap <kapelianovych.y.v@gmail.com>
2022-12-14 20:47:30 +0200

    Fix mistaken license name and rename the parameter of the catch method.

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ declare global {
       onrejected?: ((error: E) => W | Future<W, O>) | null
     ) => Future<K | W, O>;
     readonly catch: <R = T, O = E>(
-      callback?: ((error: E) => R | Future<R, O>) | null
+      onrejected?: ((error: E) => R | Future<R, O>) | null
     ) => Future<T | R, O>;
     readonly finally: (callback?: VoidFunction | null) => Future<T, E>;
   }
