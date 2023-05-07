371595b5d4715cc2fbd97c974dd51ffcc4e94b02
Yevhen Kapelianovych <kapelianovych.y.v@protonmail.com>
2023-05-07 16:03:08 +0300

    Add some type tests for the of and fail functions.

--- fail.test.ts
+++ fail.test.ts
@@ -1,9 +1,73 @@
 import test from 'node:test'
 import { ok, equal } from 'node:assert/strict'
 
-import { expectType } from 'tsd'
+import { expectNotType, expectType } from 'tsd'
 
 import Future from './index.js'
+
+{
+	const a = Future.fail('error')
+
+	// Expect the fail function to infer exact type of the non-thenable value.
+	expectType<Future.Self<never, 'error'>>(a)
+
+	a.catch(() => { })
+}
+
+{
+	const a = Future.fail(Future.of(1))
+
+	// Expect the Future to receive the Right type of the FutureLike argument as the Left type.
+	expectType<Future.Self<never, 1>>(a)
+
+	a.catch(() => { })
+}
+
+{
+	const a = Future.fail(Future.fail(1))
+
+	// Expect the Future to receive the Left type of the FutureLike argument as the Left type.
+	expectType<Future.Self<never, 1>>(a)
+
+	a.catch(() => { })
+}
+
+{
+	const a = Future.fail(Promise.resolve(1))
+
+	// Expect the Future to receive the Left type as unknown if the resolved PromiseLike is an argument.
+	expectType<Future.Self<never, unknown>>(a)
+	expectNotType<Future.Self<never, number>>(a)
+
+	a.catch(() => { })
+}
+
+{
+	const a = Future.fail<number, string>(Promise.resolve(1))
+
+	// Expect the Left type to be configurable if an argument is the resolved PromiseLike.
+	expectType<Future.Self<never, number | string>>(a)
+
+	a.catch(() => { })
+}
+
+{
+	const a = Future.fail(Promise.reject(1))
+
+	// Expect the Future to receive the Left type as unknown if the rejected PromiseLike is an argument.
+	expectType<Future.Self<never, unknown>>(a)
+
+	a.catch(() => { })
+}
+
+{
+	const a = Future.fail<string, number>(Promise.reject(1))
+
+	// Expect the Left type to be configurable if an argument is the rejected PromiseLike.
+	expectType<Future.Self<never, number | string>>(a)
+
+	a.catch(() => { })
+}
 
 test('fail function should wrap a non-Promise value into the Future (Promise)', async () => {
 	const a = Future.fail(8)
@@ -32,12 +96,3 @@ test('fail function rejects a passed Future', async (context) => {
 		return a.catch((v) => equal(v, 'mark'))
 	})
 })
-
-{
-	const a = Future.fail('error')
-
-	// Expect fail function to infer exact type of the non-thenable value.
-	expectType<Future.Self<never, 'error'>>(a)
-
-	a.catch(() => { })
-}
--- of.test.ts
+++ of.test.ts
@@ -1,12 +1,26 @@
 import test from 'node:test'
 import { ok, equal } from 'node:assert/strict'
 
-import { expectType } from 'tsd'
+import { expectNotType, expectType } from 'tsd'
 
 import Future from './index.js'
 
-// Expect the Future to have exact fulfilled type from the non-thenable value.
+// Expect the Future to have the exact Right type from the non-thenable value and never as the Left type.
 expectType<Future.Self<5, never>>(Future.of(5))
+
+{
+  const a: Future.Like<string, number> = Future.spawn(() => 'foo')
+
+  // Expect the Future to inherit Right and Left types from the FutureLike.
+  expectType<Future.Self<string, number>>(Future.of(a))
+}
+
+// Expect the Future to inherit the Right type from the PromiseLike and have the unknown as the Left type.
+expectType<Future.Self<string, unknown>>(Future.of(Promise.resolve('foo')))
+expectNotType<Future.Self<string, boolean>>(Future.of(Promise.resolve('foo')))
+
+// Left type should be configurable if the PromiseLike is an argument.
+expectType<Future.Self<string, number>>(Future.of<string, number>(Promise.resolve('foo')))
 
 test('of function should wrap a non-Promise value into the Future (Promise)', () => {
   ok(Future.of(8) instanceof Promise)
